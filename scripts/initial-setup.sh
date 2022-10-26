10b24c7dca0b42b54b41e9421b83d0c3791b2f96
Kevin Fallas <Kevin.fallas.b@gmail.com>
2022-10-26 13:48:24 -0600

    Changes to set up document and updates to website

--- scripts/initial-setup.sh
+++ scripts/initial-setup.sh
@@ -6,5 +6,14 @@ sudo apt-get install nginx -y
 sudo systemctl enable nginx
 
 #Traernos proyecto de github y colocarlo en servidor
-cd /usr/share/nginx/html
+cd tmp
+sudo git clone https://github.com/kevin-fallas-b/terraform-challenge.git
+
+#mover pagina de ubicacion temoral a nginx
+sudo mv -v terraform-challenge/website/* /var/www/html
+
+#Eliminar pagina index por defecto de nginx
+cd /var/wwww/html
+sudo rm index.nginx-debian.html
+
 
